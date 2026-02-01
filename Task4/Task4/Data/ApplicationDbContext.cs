95dc0570e998dfeddff98badd7d14041a6f5fe20
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-02-01 15:16:07 +0500

    setup database

---
+++ Task4/Task4/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Task4.Models;
+
+namespace Task4.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public virtual DbSet<User> Users { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Task4/Task4/Data/UserConfigurations.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Task4.Models;
+
+namespace Task4.Data;
+
+public class UserConfigurations : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(u => u.Id);
+
+        builder.HasIndex(u => u.Email)
+            .IsUnique();
+
+        builder
+            .Property(u => u.Name)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder
+            .Property(u => u.Email)
+            .HasMaxLength(256)
+            .UseCollation("SQL_Latin1_General_CP1_CI_AS")
+            .IsRequired();
+
+        builder
+            .Property(u => u.Status)
+            .HasConversion<string>()
+            .HasMaxLength(20)
+            .IsRequired();
+    }
+}
