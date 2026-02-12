b77ef056f975ad7dc37337f4e1e4f2792965ef7d
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-02-12 17:26:50 +0500

    fix

--- Task4/Task4/Helpers/EmailTemplateHelper.cs
+++ Task4/Task4/Helpers/EmailTemplateHelper.cs
@@ -7,13 +7,13 @@ internal static class EmailTemplateHelper
     public static string BuildConfirmationBody(string confirmationLink)
     {
         var htmlPath = Path.Combine(AppContext.BaseDirectory, "Helpers", "EmailConfirmation.html");
-        var cssPath = Path.Combine(AppContext.BaseDirectory, "wwwroot", "css", "emailStyles.css");
+        var cssPath = Path.Combine(AppContext.BaseDirectory, "wwwroot", "css", "emailstyles.css");
 
         if (!File.Exists(htmlPath))
             throw new FileNotFoundException("EmailConfirmation.html not found", htmlPath);
 
         if (!File.Exists(cssPath))
-            throw new FileNotFoundException("EmailStyles.css not found", cssPath);
+            throw new FileNotFoundException("Emailstyles.css not found", cssPath);
 
         var html = File.ReadAllText(htmlPath, Encoding.UTF8);
         var css = File.ReadAllText(cssPath, Encoding.UTF8);
@@ -26,13 +26,13 @@ internal static class EmailTemplateHelper
     public static string BuildResetPasswordBody(string resetLink)
     {
         var htmlPath = Path.Combine(AppContext.BaseDirectory, "Helpers", "ResetPassword.html");
-        var cssPath = Path.Combine(AppContext.BaseDirectory, "wwwroot", "css", "emailStyles.css");
+        var cssPath = Path.Combine(AppContext.BaseDirectory, "wwwroot", "css", "emailstyles.css");
 
         if (!File.Exists(htmlPath))
             throw new FileNotFoundException("ResetPassword.html not found", htmlPath);
 
         if (!File.Exists(cssPath))
-            throw new FileNotFoundException("emailStyles.css not found", cssPath);
+            throw new FileNotFoundException("emailstyles.css not found", cssPath);
 
         var html = File.ReadAllText(htmlPath, Encoding.UTF8);
         var css = File.ReadAllText(cssPath, Encoding.UTF8);
